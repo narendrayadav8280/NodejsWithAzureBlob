5f0c1fcc7cfccb920b0e1e31c510eafb8037322d
Narendra Kumar Yadav <er.narendra@gmail.com>
2018-05-15 18:43:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+var storage = require('azure-storage');
+var blobService = storage.createBlobService();
+var containerName = 'your-container-name';
+
+blobService.createContainerIfNotExists(containerName, function(err, result, response) {
+    if (err) {
+        console.log("Couldn't create container %s", containerName);
+        console.error(err);
+    } else {
+        if (result) {
+            console.log('Container %s created', containerName);
+        } else {
+            console.log('Container %s already exists', containerName);
+        }
+    }
+});
+
+blobService.createBlockBlobFromText(
+    containerName,
+    'my-awesome-text-blob',
+    'Hello, World!',
+    function(error, result, response){
+        if(error){
+            console.log("Couldn't upload string");
+            console.error(error);
+        } else {
+            console.log('String uploaded successfully');
+        }
+    });
+
+
+
+var blobName = 'my-awesome-text-blob';
+blobService.getBlobToText(
+    containerName,
+    blobName,
+    function(err, blobContent, blob) {
+        if (err) {
+            console.error("Couldn't download blob %s", blobName);
+            console.error(err);
+        } else {
+            console.log("Sucessfully downloaded blob %s", blobName);
+            console.log(blobContent);
+        }
+    });
